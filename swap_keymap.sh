d8554660e50f757cdd05c13270f3e2835f438635
Lluis <lluisyast@gmail.com>
2013-11-02 17:30:51 +0100

    Corrected grep pattern

--- swap_keymap.sh
+++ swap_keymap.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 # This allows me to swap my keymap to US or ES whenever I want, easily.
 
-if (setxkbmap -print | grep "+us+" 1>/dev/null 2>&1); then
+if (setxkbmap -print | grep "+us" 1>/dev/null 2>&1); then
 	setxkbmap es mac
 	echo Changed keymap to ES
 else
